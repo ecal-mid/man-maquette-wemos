eb40d89e5549d73ee15a8f70089cc6ef1c085844
martialgrin <martial.grin@gmail.com>
2024-09-05 16:30:33 +0200

    removing zoom

--- components/Controler/Sphere.js
+++ components/Controler/Sphere.js
@@ -125,31 +125,19 @@ export const Sphere = ({ id, name, data }) => {
 
 				<PerspectiveCamera position={[0, 0, 10]} />
 				<CameraControls
+					onChange={(ev) => {
+						// dollyTo(10);
+						console.log(ev);
+					}}
 					azimuthRotateSpeed={2}
-					maxDistance={0}
-					dollySpeed={0}
-					dolly={false}
-					infinityDolly={false}
-					maxZoom={0}
-					truckSpeed={0}
-					minZoom={0}
-					boundaryEnclosesCamera={true}
+					maxDistance={5}
+					minDistance={5}
 				/>
 				<ambientLight intensity={1} />
 				<directionalLight position={[10, 10, 5]} intensity={1} />
 				<mesh>
 					<sphereGeometry args={[2, 100, 100]} />
-					<meshBasicMaterial map={colorMap}>
-						{/* <GradientTexture
-							stops={[0, 0.25, 0.5, 1]} // As many stops as you want
-							colors={["white", "blue", "rgb(0,255,0)", "red"]} // Colors need to match the number of stops
-							size={1024} // Size is optional, default = 1024
-							width={1024} // Width of the canvas producing the texture, default = 16
-							type={GradientType.Radial} // The type of the gradient, default = GradientType.Linear
-							innerCircleRadius={0} // Optional, the radius of the inner circle of the gradient, default = 0
-							outerCircleRadius={"auto"} // Optional, the radius of the outer circle of the gradient, default = auto
-						/> */}
-					</meshBasicMaterial>
+					<meshBasicMaterial map={colorMap}></meshBasicMaterial>
 				</mesh>
 				{/* </PresentationControls> */}
 			</Canvas>
